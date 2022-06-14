d932b198e9b4c3776cc11f8f6cbaf7c2d2ad106d
drjackherer <mongkolpersonal@gmail.com>
2022-06-14 11:59:45 -0700

    Add project files.

---
+++ BankEncapulation/BankAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapulation
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+
+        }
+
+        private double _balance = 10;
+        
+        public double Deposit(double amount)
+        {
+          return _balance += amount;
+        }
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+
+
+
+        
+    }
+
+}
---
+++ BankEncapulation/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BankEncapulation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var bankAccount = new BankAccount();
+            Console.WriteLine($"Showing balance {bankAccount.GetBalance()}");
+        }
+    }
+}
